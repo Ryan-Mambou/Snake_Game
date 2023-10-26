08019e42dfc7dc94af56b58885cd687c88c521e3
Ryan Djemtchemeu <ryanmambou@gmail.com>
2023-10-26 09:46:20 +0100

    moving snake on direction press

--- main.py
+++ main.py
@@ -8,15 +8,20 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
 
 game_on = True
-
-snake = Snake()
 
 while game_on:
     screen.update()
     time.sleep(0.1)
-    snake.move_snake()
+    snake.move()
 
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,12 +1,19 @@
-from turtle import  Turtle
+from turtle import Turtle
+
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 SNAKE_STEP = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
-
+        # Notice it is placed after the creation of the snake
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -17,11 +24,25 @@ class Snake:
             new_turtle.goto(position)
             self.segments.append(new_turtle)
 
-
-    def move_snake(self):
+    def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.segments[0].forward(SNAKE_STEP)
 
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(90)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(0)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(180)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(270)
