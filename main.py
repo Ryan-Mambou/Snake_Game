f718558ecfa001ed6059bf14627b1922034b1835
Ryan Djemtchemeu <ryanmambou@gmail.com>
2023-11-06 09:23:24 +0100

    create scoreboard and food components

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,6 +26,14 @@ while game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increment_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() >  280 or snake.head.ycor() < -280:
+        game_on = False
+        scoreboard.game_over()
 
 
 screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+
+class ScoreBoard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 300)
+        self.update_score()
+        self.hideturtle()
+
+    def update_score(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increment_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
