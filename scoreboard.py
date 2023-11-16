05ea59272c3906934ca9e98fcf37f4b06cc824a8
Ryan Djemtchemeu <ryanmambou@gmail.com>
2023-11-16 10:17:37 +0100

    having a record of highscore

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,9 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('high_score.txt') as file:
+            high_score = file.read()
+            self.high_score = int(high_score)
         self.color("white")
         self.penup()
         self.goto(0, 265)
@@ -23,9 +25,16 @@ class ScoreBoard(Turtle):
     def reset_score(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.update_high_score()
         self.score = 0
         self.update_score()
 
     def increment_score(self):
         self.score += 1
         self.update_score()
+
+    def update_high_score(self):
+        with open('high_score.txt', mode='w') as file:
+            # file.truncate(0)
+            file.write(f"{self.score}")
+
